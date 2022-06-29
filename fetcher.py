e77c0a17341665a1535070ef94284868b4fa16ae
Utshaan <Utkarsh.thanvi@gmail.com>
2022-06-30 00:50:51 +0530

    started reverse engineering the rarbgapi python wrapper

--- fetcher.py
+++ fetcher.py
@@ -1,22 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
-from rich import print, table
+import rarbgapi
 
-# x = input("What do you wanna watch today: ")
+client = rarbgapi.RarbgAPI()
 
-# x = x.replace(" ", "+")
-
-# cookies = dict( aby='2', expla='1', expla3='1', gaDts48g='q8h5pp9t', ppu_delay_9ef78edf998c4df1e1636c9a474d9f47='1', ppu_main_9ef78edf998c4df1e1636c9a474d9f47='1', ppu_sub_9ef78edf998c4df1e1636c9a474d9f47='3', skt='FZMSYi9dub', use_alt_cdn='1')
-
-# header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'}
-
-# r = requests.get(f"https://rarbgprx.org/torrents.php?search={x}", headers=header, cookies=cookies)
-
-# soup = BeautifulSoup(r.text, features='html.parser')
-
-# print(soup.prettify())
-
-with open("h.html", 'r') as file:
-    soup = BeautifulSoup(file, 'html.parser')
-
-print(soup.prettify())
+for t in client.search(search_string="dr strange"):
+    print(t)
