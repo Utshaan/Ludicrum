d4f8fff919be5e36c1dacdb921ff68ddafb4fa19
Utshaan <Utkarsh.thanvi@gmail.com>
2022-07-05 02:21:09 +0530

    Started using insomnia ^| work on direct api

--- fetcher.py
+++ fetcher.py
@@ -1,6 +1,38 @@
-import rarbgapi
+import requests
+from rich import print
+from bs4 import BeautifulSoup
+import platform
 
-client = rarbgapi.RarbgAPI()
+# x = '+'.join(input().split())
 
-for t in client.search(search_string="dr strange"):
-    print(t)
+scrapi = 'https://torrentapi.org/pubapi_v2.php'
+
+def _get_user_agent():
+    uname = '; '.join(platform.uname())
+    pyver = platform.python_version()
+    return f'Ludicrum/0.1.0 ({uname}) python {pyver}'
+
+params_token = {'get_token': 'get_token', 'app_id': 'Ludicrum'}
+headers_token = {'user-agent': _get_user_agent()}
+
+session = requests.Session()
+request = requests.Request('Get', scrapi, params=params_token, headers=headers_token)
+preq = request.prepare()
+response = session.send(preq)
+response.raise_for_status()
+
+token = response
+print(token.json()['token'])
+
+params_query = {
+    'mode': 'search',
+    'token': token.json()['token']
+}
+
+
+
+# r = requests.get(f'https://rarbgprx.org/torrents.php?search={x}').text
+# soup = BeautifulSoup(r, 'html.parser')
+
+# print(soup.prettify())
+
--- test.py
+++ test.py
@@ -17,3 +17,69 @@ resp = session.send(preq)
 resp.raise_for_status()
 
 print(resp)
+
+class Ludicrum():
+
+    APP_ID = 'rarbgapi'
+
+    def __init__(self) -> None:
+        self._token = None
+        self._scrape_site = "http://torrentapi.org/pubapi_v2.php"
+    
+    def user_agent(self) -> str:
+        username = '; '.join(platform.uname())
+        python_version = platform.python_version()
+        return f'{self.APP_ID}/0.5.0 ({username}) python {python_version}'
+    
+    def token(self):
+        params = {
+            'get_token': 'get_token'
+        }
+        return self.requests('Get', self._scrape_site, params)
+    
+    def query(self, mode, **kwargs):
+        params = {
+            'mode': mode,
+            'token': self.token
+        }
+
+        if 'extended_response' in kwargs:
+            params['format'] = 'json_extended' \
+                if kwargs['extended_response'] else 'json'
+            del kwargs['extended_response']
+        
+        if 'categories' in kwargs:
+            params['category'] = ';'.join(
+                [str(c) for c in kwargs['categories']])
+            del kwargs['categories']
+        
+        for key, value in kwargs.items():
+            if key not in [
+                'sort', 'limit', 'search_string', 'search_imdb', 'search_tvdb', 'search_themoviedb',
+            ]:
+                raise ValueError(f'unsupported parameter {key}')
+            
+            if value in None:
+                continue
+
+            params[key] = value
+        
+        return self.requests('Get', self._scrape_site, params)
+
+    def requests(self, method, url, params={}):
+        params.update({
+            'app_id': self.APP_ID
+        })
+
+        headers = {'user-agent': self.user_agent()}
+
+        session = requests.Session()
+        request = requests.Request(method, url, params=params, headers=headers)
+        prequest = req.prepare()
+        response = session.send(prequest)
+        response.raise_for_status()
+        return response
+
+
+client = Ludicrum()
+client.
