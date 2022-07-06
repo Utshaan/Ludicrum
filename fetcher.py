414c9e58cba592b2c9184129350e5319e4d43003
Utshaan <Utkarsh.thanvi@gmail.com>
2022-07-06 21:05:14 +0530

    Added LudicrousTorrent class

--- fetcher.py
+++ fetcher.py
@@ -1,42 +1,42 @@
-from time import sleep
-import requests
-from rich import print
-from bs4 import BeautifulSoup
-import platform
+# from time import sleep
+# import requests
+# from rich import print
+# from bs4 import BeautifulSoup
+# import platform
 
-# x = '+'.join(input().split())
+# # x = '+'.join(input().split())
 
-scrapi = 'https://torrentapi.org/pubapi_v2.php'
+# scrapi = 'https://torrentapi.org/pubapi_v2.php'
 
-def _get_user_agent():
-    username = 'Omninight'
-    return 'Ludicrum/0.1.0 {username}'
+# def _get_user_agent():
+#     username = 'Omninight'
+#     return 'Ludicrum/0.1.0 {username}'
 
-params_token = {'get_token': 'get_token', 'app_id': 'Ludicrum'}
-headers_token = {'user-agent': _get_user_agent()}
+# params_token = {'get_token': 'get_token', 'app_id': 'Ludicrum'}
+# headers_token = {'user-agent': _get_user_agent()}
 
-session = requests.Session()
-request = requests.Request('Get', scrapi, params=params_token, headers=headers_token)
-preq = request.prepare()
-response = session.send(preq)
-response.raise_for_status()
+# session = requests.Session()
+# request = requests.Request('Get', scrapi, params=params_token, headers=headers_token)
+# preq = request.prepare()
+# response = session.send(preq)
+# response.raise_for_status()
 
-token = response
-# print(token.json())
+# token = response
+# # print(token.json())
 
-mode = input('What do you wanna do: ')
-search = input('Name the show: ')
+# mode = input('What do you wanna do: ')
+# search = input('Name the show: ')
 
-params_query = {
-    'app_id': 'Ludicrum',
-    'mode': mode,
-    'token': token.json()['token'],
-    'search_string': search
-}
+# params_query = {
+#     'app_id': 'Ludicrum',
+#     'mode': mode,
+#     'token': token.json()['token'],
+#     'search_string': search
+# }
 
-request = requests.Request('Get', scrapi, params=params_query, headers=headers_token)
-preq = request.prepare()
-response = session.send(preq)
-response.raise_for_status()
+# request = requests.Request('Get', scrapi, params=params_query, headers=headers_token)
+# preq = request.prepare()
+# response = session.send(preq)
+# response.raise_for_status()
 
-print(response.json())
+# print(response.json())
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from time import sleep
 import requests
-from rich import print
+from rich import print, table
+from bs4 import BeautifulSoup
 
 class Ludicrum():
     DEFAULT_TRIES = 5
@@ -11,7 +12,7 @@ class Ludicrum():
         self.APP_ID = 'Ludicrum'
         self.Rsession = requests.Session()
         self.TOKEN = self.token()
-        self.DEFAULT_TRIES = 5
+        # self.DEFAULT_TRIES = 5
     
     def r(self, params):
         req = requests.Request('Get', self.SCRAPI, params=params, headers=self.HEADER)
@@ -29,31 +30,67 @@ class Ludicrum():
             'app_id': self.APP_ID,
             'mode': 'search',
             'token': self.TOKEN,
-            'search_string': string
+            'search_string': string,
+            'format': 'json_extended',
+            # 'ranked': 0
         }
         response = self.r(params).json()
         error_code = response.get('error_code')
         if error_code and tries > 0:
             match error_code:
                 case 20:
-                    print('Sitey problem...?')
-                    sleep(2**min(tries- 4,0) + 2)
+                    sleep(2)
                     return self.search(string, tries - 1)
                 case _:
-                    print(error_code)
-        elif tries < 0:
-            raise Exception('Something went wrong')
-        else:
+                    return error_code
+        if not error_code:
             return response
+        else:
+            print(response)
+            return {'torrent_results': ''}
+
+class LudicrousTorrent():
+    def __init__(self, info):
+        self.title = info['title']
+        self.category = info['category']
+        self.link = info['download']
+        self.seeders = info['seeders']
+        self.leechers = info['leechers']
+        self.size = round(int(info['size'])/(2**30),2)
+        self.published_date = info['pubdate']
+        self.episode_info = info['episode_info']
+        self.rank = info['ranked']
+        self.download = False
+        
 
 
 client = Ludicrum('Omninight')
-results = client.search('stranger things')['torrent_results']
+ask = '+'.join(input("Search: ").split())
 
-for result in results:
-    name = result['filename']
-    category = result['category']
-    link = result['download']
-    print(name, category, link)
+# x = requests.get(f'https://thetvdb.com/search?query={ask}')
+# soup = BeautifulSoup(x.text, 'html.parser')
+# print(soup)
+# raise SystemExit
 
+
+results = client.search(ask)['torrent_results']
+
+datas = [print(f'[yellow]{LudicrousTorrent(result)}[/yellow]') for result in results]
+
+
+# """{
+#         'title': 'Ms.Marvel.S01E05.2160p.DSNP.WEB-DL.DDP5.1.Atmos.DV.MP4.x265-DVSUX[rartv]',
+#         'category': 'Movies/TV-UHD-episodes',
+#         'download': 'magnet:?xt=urn:btih:33dc4fcdcc078e5b1512fab87d93712d38614cf2&dn=Ms.Marvel.S01E05.2160p.DSNP.WEB-DL.DDP5.1.Atmos.DV.MP4.x265-DV
+# SUX%5Brartv%5D&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2860&tr=udp%3A%2F%2F9.rarbg.to%3A2780&tr=udp%3A%2
+# F%2Ftracker.tallpenguin.org%3A15770&tr=udp%3A%2F%2Ftracker.slowcheetah.org%3A14780',
+#         'seeders': 70,
+#         'leechers': 38,
+#         'size': 4770025745,
+#         'pubdate': '2022-07-06 07:42:57 +0000',
+#         'episode_info': {'imdb': 'tt10857164', 'tvrage': None, 'tvdb': '368612', 'themoviedb': '92782'},
+#         'ranked': 1,
+#         'info_page': 'https://torrentapi.org/redirect_to_info.php?token=ynoq2zfx4p&p=2_8_1_9_8_0_2__33dc4fcdcc'
+#     }
+# """
 
