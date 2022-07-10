c0332c5dae27d498dba8132737d36c2bf052798f
Utshaan <Utkarsh.thanvi@gmail.com>
2022-07-11 01:44:58 +0530

    Updated Requirements.txt | Added API class
    Configured method to add transfers on webUI for tixati
    Made new session for authorisation of webUI

--- TixAPI.py
+++ TixAPI.py
@@ -1,1 +1,30 @@
 #tixati api work here 
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+username = 'omninight'
+
+class TixatiAPI():
+    def __init__(self, username) -> None:
+        self.target_site = "https://localhost:port"
+        self.session = requests.Session()
+        self.session.verify= False
+        self.username = username
+    
+    def auth(self):
+        self.session.auth = requests.auth.HTTPDigestAuth('omninight', 'nimda')
+    
+    def get_home(self):
+        return self.session.get(f'{self.target_site}/home').text
+    
+    def get_transfers(self):
+        return self.session.get(f'{self.target_site}/transfers').text
+    
+    def add_magnet_transfer(self, link):
+        self.session.post(f'{self.target_site}/transfers/action', {'addlinktext': link, "addlink": 'add'})
+
+
+dclient = TixatiAPI(username)
+
+dclient.auth()
