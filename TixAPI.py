8d6f011bd618c19b27b2492392f7d3336cd18f5c
Utshaan <Utkarsh.thanvi@gmail.com>
2022-07-12 01:56:25 +0530

    Started formatting data into tables
    Added homestat data. Need to work on arranging data,
    to make it more accesible from terminal.
    
    To do:
     - look into Textual frameworks
     - test out rich.pagers / curses

--- TixAPI.py
+++ TixAPI.py
@@ -1,13 +1,23 @@
 #tixati api work here 
 import requests
 import urllib3
+import json
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 username = 'omninight'
+console = Console()
 
 class TixatiAPI():
     def __init__(self, username) -> None:
-        self.target_site = "https://localhost:port"
+        with open('config.json', 'r') as f:
+            file = json.load(f)
+            self.port = file['port']
+        
+        self.target_site = f"https://localhost:{self.port}"
         self.session = requests.Session()
         self.session.verify= False
         self.username = username
@@ -24,7 +34,30 @@ class TixatiAPI():
     def add_magnet_transfer(self, link):
         self.session.post(f'{self.target_site}/transfers/action', {'addlinktext': link, "addlink": 'add'})
 
+class RequestScraper():
+    def __init__(self, text) -> None:
+        self.soup = BeautifulSoup(text, 'html.parser')
+    
+    def get_table(self):
+        tables = self.soup.find_all('table')
 
-dclient = TixatiAPI(username)
+        table_2_rows = tables[1].find_all('tr')[1:]
 
-dclient.auth()
+        table_2 = Table(title=tables[1].attrs['class'][0], expand=True)
+
+        for i in tables[1].find_all('th'):
+            table_2.add_column(i.text)
+        
+        for row in table_2_rows:
+            l = row.find_all('td')
+            table_2.add_row(l[0].text[:-len(l[0].span.text)], l[1].text[:-len(l[1].span.text)], l[2].text[:-len(l[2].span.text)])
+
+        console.print(table_2)
+        # print(table_2_rows)
+
+
+client = TixatiAPI(username)
+
+client.auth()
+scraper = RequestScraper(client.get_home())
+scraper.get_table()
