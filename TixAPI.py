c6f7b22963dc4184dd74facfd14d1738c5a6dc2e
Utshaan <Utkarsh.thanvi@gmail.com>
2022-07-13 17:31:02 +0530

    Figured out table in table problem
    Added the second table from the WebUI to the software

--- TixAPI.py
+++ TixAPI.py
@@ -25,10 +25,10 @@ class TixatiAPI():
     def auth(self):
         self.session.auth = requests.auth.HTTPDigestAuth('omninight', 'nimda')
     
-    def get_home(self):
+    def get_home(self) -> str:
         return self.session.get(f'{self.target_site}/home').text
     
-    def get_transfers(self):
+    def get_transfers(self) -> str:
         return self.session.get(f'{self.target_site}/transfers').text
     
     def add_magnet_transfer(self, link):
@@ -42,18 +42,26 @@ class RequestScraper():
         tables = self.soup.find_all('table')
 
         table_2_rows = tables[1].find_all('tr')[1:]
-
-        table_2 = Table(title=tables[1].attrs['class'][0], expand=True)
+        
+        table_2 = Table(title=tables[1].attrs['class'][0].upper(), expand=True, header_style="#bb546a",border_style="#395013", title_style='cyan', padding=0)
 
         for i in tables[1].find_all('th'):
             table_2.add_column(i.text)
         
+        mini_tables = [Table() for i in range(3)]
+        for table in mini_tables:
+            table.add_column('Item')
+            table.add_column('Value', justify='right')
+            table.expand = True
+            table.box = None
+
         for row in table_2_rows:
             l = row.find_all('td')
-            table_2.add_row(l[0].text[:-len(l[0].span.text)], l[1].text[:-len(l[1].span.text)], l[2].text[:-len(l[2].span.text)])
-
+            for i,data in enumerate(l):
+                mini_tables[i].add_row(data.next.text, data.next.next.text)
+            
+        table_2.add_row(mini_tables[0], mini_tables[1], mini_tables[2])
         console.print(table_2)
-        # print(table_2_rows)
 
 
 client = TixatiAPI(username)
